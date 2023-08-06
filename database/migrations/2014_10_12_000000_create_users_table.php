eb64ac20e0c00c902b405fa56456e471ad560749
Nanu Oghenetega <nanumichael27@gmail.com>
2023-08-06 23:18:20 +0100

    setup migrations and seeders to start

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name')->nullable();
-            $table->string('id')->nullable();
+            $table->string('code')->nullable();
             $table->enum('role', ['admin', 'voter'])->default('voter');
             $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
