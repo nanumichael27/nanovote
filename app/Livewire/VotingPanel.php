44af2cfc6b37b474f927573f1f61aa8432729493
Nanu Oghenetega <nanumichael27@gmail.com>
2023-08-10 08:41:23 +0100

    WIP started with election wizard

--- app/Livewire/VotingPanel.php
+++ app/Livewire/VotingPanel.php
@@ -6,6 +6,12 @@ use Livewire\Component;
 
 class VotingPanel extends Component
 {
+    public $election;
+    public $voter;
+    public function mount() {
+        $voter = auth()->user();
+        $this->election = $voter->election;
+    }
     public function render()
     {
         return view('livewire.voting-panel');
--- resources/views/livewire/voting-panel.blade.php
+++ resources/views/livewire/voting-panel.blade.php
@@ -1,8 +1,21 @@
 <div class="p-6 text-gray-900">
-    @if (!Auth::user()->election)
+    @if (!$election)
         {{ __("You have not been assigned to vote") }}
     @else
-        {{ __("You are cleared for voting")}}
+        @foreach ($election->offices as $office)
+            <div class="bg-stone-300 rounded p-3 my-5 shadow-2xl">
+                <h3 class="text-2xl">{{$office->title}}</h3>
+                <h6 class="text-lg font-medium">candidates:</h6>
+                @foreach ($office->candidates as $candidate)
+                <div class="bg-white rounded p-5 my-3 cursor-pointer hover:bg-green-100 active:bg-green-300 text-center">
+                    <div><img src="{{$candidate->getFirstMediaUrl('images')}}" class="w-1/2 mx-auto my-0" alt=""></div>
+                    <div>First Name: {{$candidate->first_name}}</div>
+                    <div>Last Name: {{$candidate->last_name}}</div>
+                    <div>Part: {{$candidate->part}}</div>
+                </div>
+                @endforeach
+            </div>
+        @endforeach
     @endif
 
 </div>
