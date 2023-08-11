6dd6158399e5d6be5f9c5f9ca489fd4ba5ef6390
Nanu Oghenetega <nanumichael27@gmail.com>
2023-08-11 01:02:55 +0100

    setup voting wizard

--- app/Livewire/VotingPanel.php
+++ app/Livewire/VotingPanel.php
@@ -2,16 +2,53 @@
 
 namespace App\Livewire;
 
+use App\Models\Office;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class VotingPanel extends Component
 {
     public $election;
+
+    public $office;
+    public $step;
     public $voter;
+
+    public $votes = [];
+
     public function mount() {
+        $this->step = 0;
         $voter = auth()->user();
         $this->election = $voter->election;
+        $this->office = $this->election->offices[$this->step];
     }
+
+    public function nextStep() {
+        if($this->step < $this->election->offices->count() - 1) {
+            $this->step++;
+        }
+        $this->office = $this->election->offices[$this->step];
+    }
+
+    public function previousStep() {
+        if($this->step > 0) {
+            $this->step--;
+        }
+        $this->office = $this->election->offices[$this->step];
+    }
+    public function castVote($office, $candidate) {
+        $this->votes[$office] = $candidate;
+        $this->nextStep();
+    }
+
+    #[Computed]
+    public function isLastStep() {
+        return $this->step == $this->election->offices->count() - 1;
+    }
+
+
+
+
     public function render()
     {
         return view('livewire.voting-panel');
--- resources/views/livewire/voting-panel.blade.php
+++ resources/views/livewire/voting-panel.blade.php
@@ -1,21 +1,33 @@
-<div class="p-6 text-gray-900">
-    @if (!$election)
-        {{ __("You have not been assigned to vote") }}
-    @else
-        @foreach ($election->offices as $office)
-            <div class="bg-stone-300 rounded p-3 my-5 shadow-2xl">
-                <h3 class="text-2xl">{{$office->title}}</h3>
-                <h6 class="text-lg font-medium">candidates:</h6>
-                @foreach ($office->candidates as $candidate)
-                <div class="bg-white rounded p-5 my-3 cursor-pointer hover:bg-green-100 active:bg-green-300 text-center">
-                    <div><img src="{{$candidate->getFirstMediaUrl('images')}}" class="w-1/2 mx-auto my-0" alt=""></div>
-                    <div>First Name: {{$candidate->first_name}}</div>
-                    <div>Last Name: {{$candidate->last_name}}</div>
-                    <div>Part: {{$candidate->part}}</div>
+<div>
+    <div class="p-6 text-gray-900">
+        @if (!$election)
+            {{ __("You have not been assigned to vote") }}
+        @else
+        <div class="bg-stone-200 rounded p-3 my-5 shadow-sm flex justify-between">
+          <div class="bg-white p-3 rounded hover:bg-green-100 cursor-pointer" wire:click="previousStep()">Prev</div>
+          @if ($this->isLastStep)
+          <div class="bg-white p-3 rounded hover:bg-green-100 cursor-pointer">finish</div>
+          @else
+          <div class="bg-white p-3 rounded hover:bg-green-100 cursor-pointer" wire:click="nextStep()">Next</div>
+          @endif
+          
+        </div>
+                <div class="bg-stone-200 rounded p-5 my-5 shadow-sm">
+                    <h3 class="text-3xl font-extrabold">{{$office->title}}</h3>
+                    <div class="grid lg:grid-cols-3 sm:grid-cols-1 gap-2 text-center">
+                        @foreach ($office->candidates as $candidate)
+                        <div class="bg-white rounded-lg px-5 py-16 my-3 cursor-pointer 
+                        active:bg-green-300 {{in_array($candidate->id, $votes)? 'bg-green-300' : 'hover:bg-green-100'}}" wire:click="castVote({{$office->id}}, {{$candidate->id}})">
+                            <div><img src="{{$candidate->getFirstMediaUrl('images')}}" class="w-2/3 mx-auto my-0 rounded-2xl shadow-lg" alt=""></div>
+                            <div class="mx-auto my-3 text-center">
+                                <div class="text-xl font-extrabold "> {{$candidate->first_name}} {{$candidate->last_name}}</div>
+                                <div class="text-lg font-bold text-gray-600"> {{$candidate->part}}</div>
+                            </div>
+                        </div>
+                        @endforeach
+                    </div>
                 </div>
-                @endforeach
-            </div>
-        @endforeach
-    @endif
-
+        @endif
+    
+    </div>
 </div>
